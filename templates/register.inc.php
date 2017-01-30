11a15cb7c27a7f048b3ede8c1b80550a5d35345b
freddy2001 <kai.kleinheuer@outlook.com>
2017-01-30 13:48:11 +0100

    Update register.inc.php

--- templates/register.inc.php
+++ templates/register.inc.php
@@ -2,11 +2,11 @@ Vielen Dank für dein Interesse an der MKG Technik AG App. <br>
 Bitte fülle folgendes Formular aus, um Zugriff zu erhalten, oder verbinde dich mit Deinem Facebook-Account.<br>
 <form>
 	Vorname:	
-	<input type="text" placeholder="Wiard">
+	<input type="text" placeholder="Vorname">
 	Nachname:
-	<input type="text" placeholder="Cassens"><br>
+	<input type="text" placeholder="Nachname"><br>
 	Benutzername:
-	<input type="text" placeholder="wiardcs"><br>
+	<input type="text" placeholder="username"><br>
 	<small>Der Benutzername darf nur kleinbuchstaben und zahlen enthalten und wird zur Anmeldung benötigt.</small><br/>
 	E-Mail:
 	<input type="email" placeholder="email"><br>
