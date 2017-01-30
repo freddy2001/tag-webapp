6385ec37126e6b29f274e31d71d382f5d7aad404
freddy2001 <freddy2001@outlook.de>
2017-01-30 19:02:37 +0100

    auslagerung

--- templates/register.inc.php
+++ templates/register.inc.php
@@ -14,5 +14,15 @@ Bitte fülle folgendes Formular aus, um Zugriff zu erhalten, oder verbinde dich 
 	<input type="phone"><br>
 	
 </form>
-
-Mit einem Klick auf Anmelden versicherst du, dass du mit den Datenschutzbedingungen einverstanden bist.
+<br>
+<br>
+<br>
+<p>Durch Klicken auf die Schaltfläche "Anmelden" bestätige ich, dass ich mit folgenden Datenschutzbedingungen einverstanden bin:
+<ul>
+<li>Die von mir zur Identifizierung gesammelten Datenfelder wie Vorname, Nachname und Profilbild auf dem Server der Technik AG gespeichert werden und von allen angemeldeten Mitgliedern gesehen werden knnen.</li>
+<li>Die Daten werden auf einem Server mit Standort Deutschland gespeichert. </li>
+<li>Eine eingegebene Telefonnummer kann an einen externen Dienstleister weitergegeben werden, um Dir SMS Benachrichtigungen zu schicken. </li>
+<li>Durch das Absenden von Posts werden die Daten Vorname, Nachname und Postinhalt an die Server der Facebook, Inc. weitergeleitet, deren Datenschutzbedingungen auf facebook.com eingesehen werden können. </li>
+<li>Zur Datenbank haben nur folgende Mitglieder der Technik-AG Zugriff: </li>
+<li>Daten wie dein Passwort werden verschlüsselt abgelegt und die Übertragung zum Server erfolgt über eine gesichterte TLS-Verbindung</li>
+</ul></p>
