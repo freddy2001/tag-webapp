1e905270719c771786b06abc113ada07050aa782
freddy2001 <freddy2001@outlook.de>
2017-01-30 18:54:08 +0100

    html

--- templates/datenschutz.inc.php
+++ templates/datenschutz.inc.php
@@ -1,4 +1,6 @@
-Mit einem Klick auf die Schaltflche "Anmelden" besttige ich, dass ich mit folgenden Datenschutzbedingungen einverstanden bin:
-* Die von mir zur Identifizierung gesammelten Datenfelder wie Vorname, Nachname und Profilbild auf dem Server der Technik AG gespeichert werden und von allen angemeldeten Mitgliedern gesehen werden knnen.
-* Die Daten auf einem Server mit Standort Deutschland verwaltet werden. 
-* Eine Eingegebene Telefonnummer an einen externen Dienstleister weitergegeben werden kann, um Dir SMS Benachrichtigungen zu schicken. 
+Durch Klicken auf die Schaltfläche "Anmelden" bestätige ich, dass ich mit folgenden Datenschutzbedingungen einverstanden bin:
+<ul>
+<li>Die von mir zur Identifizierung gesammelten Datenfelder wie Vorname, Nachname und Profilbild auf dem Server der Technik AG gespeichert werden und von allen angemeldeten Mitgliedern gesehen werden knnen.</li>
+<li>Die Daten auf einem Server mit Standort Deutschland verwaltet werden. </li>
+<li>Eine Eingegebene Telefonnummer an einen externen Dienstleister weitergegeben werden kann, um Dir SMS Benachrichtigungen zu schicken. </li>
+</ul>
