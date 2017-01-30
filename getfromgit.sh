73a49efea29393f63afc10e34c24c5aac310c3f9
freddy2001 <freddy2001@outlook.de>
2017-01-30 16:14:08 +0100

    fix
    fix

--- getfromgit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd /var/www/test_html/tag-webapp && /usr/bin/git pull 2>&1
--- gitupdate.php
+++ gitupdate.php
@@ -1,3 +1,3 @@
 <?php
-echo shell_exec("./getfromgit.sh");
+echo shell_exec("cd /var/www/test_html/tag-webapp && /usr/bin/git pull 2>&1");
 ?>
